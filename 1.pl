062850782e74f79be0faa64353fc891518cdf03e
Matt Schmiermund <mtschmie@gmail.com>
2013-06-23 16:03:57 -0400

    Testing this git thing out

---
+++ 1.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use List::Util 'sum';
+my @goodNums = (0);
+
+for ( my $i = 1; $i<1000; $i++) {
+    if(!($i % 3) || !($i % 5)) {
+	#print "Found one - " . $i;
+	#print "\n";
+	push (@goodNums, $i);
+    }
+}
+print "Loop output is - \n";
+my $tots = sum @goodNums;
+print $tots;
+print "\n\n";
+
+my @smartNums = ( 1 .. 999 );
+
+@smartNums = grep { !($_ %3) || !($_ %5) } @smartNums;
+print "Grep smart array output is - \n";
+print sum @smartNums;
+print "\n\n\n";
+
+
