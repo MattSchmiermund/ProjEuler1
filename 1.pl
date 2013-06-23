821436584b5d2e1ffdafa5fc7fe8c7aaa92cc9eb
MattSchmiermund <mtschmie@gmail.com>
2013-06-23 16:10:07 -0400

    Testing this git thing out again

--- 1.pl
+++ 1.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use List::Util 'sum';
 my @goodNums = (0);
-
+#loop approach
 for ( my $i = 1; $i<1000; $i++) {
     if(!($i % 3) || !($i % 5)) {
 	#print "Found one - " . $i;
@@ -17,8 +17,8 @@ my $tots = sum @goodNums;
 print $tots;
 print "\n\n";
 
+#grep approach
 my @smartNums = ( 1 .. 999 );
-
 @smartNums = grep { !($_ %3) || !($_ %5) } @smartNums;
 print "Grep smart array output is - \n";
 print sum @smartNums;
